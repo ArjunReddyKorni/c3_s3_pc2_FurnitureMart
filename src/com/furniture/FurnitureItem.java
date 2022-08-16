13bb8f03b9ba64873160c8f2148e037da8260ad3
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-08-16 16:42:49 +0530

    Initial commit

---
+++ src/com/furniture/FurnitureItem.java
@@ -0,0 +1,70 @@
+package com.furniture;
+
+public class FurnitureItem {
+
+    private int furnitureCode;
+    private String furnitureType;
+    private String gradeOfFurniture;
+    private String colour;
+    private String furnitureUsage;
+    private double price;
+    static final int DISCOUNT =5;
+
+    public FurnitureItem(){
+
+    }
+
+    public int getFurnitureCode() {
+        return furnitureCode;
+    }
+
+    public void setFurnitureCode(int furnitureCode) {
+        this.furnitureCode = furnitureCode;
+    }
+
+    public void setFurnitureType(String furnitureType) {
+        this.furnitureType = furnitureType;
+    }
+
+    public String getFurnitureType() {
+        return furnitureType;
+    }
+
+    public void setGradeOfFurniture(String gradeOfFurniture) {
+        this.gradeOfFurniture = gradeOfFurniture;
+    }
+
+    public String getGradeOfFurniture() {
+        return gradeOfFurniture;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setFurnitureUsage(String furnitureUsage) {
+        this.furnitureUsage = furnitureUsage;
+    }
+
+    public String getFurnitureUsage() {
+        return furnitureUsage;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public float  calculateDISCOUNT() {
+
+        if(gradeOfFurniture.equalsIgnoreCase("grade1") && furnitureUsage.equalsIgnoreCase("outdoor"))
+        return DISCOUNT;
+    }
+}
